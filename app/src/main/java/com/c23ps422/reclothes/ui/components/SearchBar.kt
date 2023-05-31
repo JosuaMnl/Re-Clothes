102c0a3c6a8828e25dfb7851ed22a772a3fca034
JosuaMnl <josuamnalu@gmail.com>
2023-05-31 17:52:27 +0700

    Make Component Search Bar

---
+++ app/src/main/java/com/c23ps422/reclothes/ui/components/SearchBar.kt
@@ -0,0 +1,15 @@
+package com.c23ps422.reclothes.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SearchBar() {
+    
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    
+}

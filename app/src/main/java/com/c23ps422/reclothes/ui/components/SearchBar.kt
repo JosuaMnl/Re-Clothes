cd9571f1dc7735cd5daf0a934bae5d89434270d8
JosuaMnl <josuamnalu@gmail.com>
2023-05-31 18:46:16 +0700

    Make component Search Bar

--- app/src/main/java/com/c23ps422/reclothes/ui/components/SearchBar.kt
+++ app/src/main/java/com/c23ps422/reclothes/ui/components/SearchBar.kt
@@ -1,15 +1,74 @@
 package com.c23ps422.reclothes.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.c23ps422.reclothes.R
+import com.c23ps422.reclothes.ui.theme.ReClothesTheme
 
 @Composable
-fun SearchBar() {
-    
+fun SearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TextField(
+        value = query,
+        onValueChange = onQueryChange,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        trailingIcon = {
+            Icon(
+                modifier = Modifier.size(18.dp),
+                imageVector = Icons.Default.Close,
+                contentDescription = null
+            )
+        },
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+            .clip(RoundedCornerShape(16.dp)),
+    )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun SearchBarPreview() {
-    
+    ReClothesTheme {
+        SearchBar(
+            query = "",
+            onQueryChange = {}
+        )
+    }
 }
